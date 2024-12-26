6c6a57cc640c1be5295df6295f0dac588d4698a0
Yoonkyung Park <ygpark@nextree.io>
2024-12-26 22:56:42 +0900

    Main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,25 +8,21 @@ import ProjectDetail3 from "./pages/ProjectDetail3";
 import Contact from "../components/Footer";
 import DefaultLayout from "./layouts/Default";
 
-const router = createBrowserRouter(
-  [
-    {
-      element: <DefaultLayout />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "skill", element: <Skill /> },
-        { path: "project", element: <Project /> },
-        { path: "contact", element: <Contact /> },
-        { path: "project-detail", element: <ProjectDetail /> },
-        { path: "project-detail2", element: <ProjectDetail2 /> },
-        { path: "project-detail3", element: <ProjectDetail3 /> },
-      ],
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/my-home", // GitHub Pages 배포 경로 설정
-  }
-);
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/skill",
+        element: <Skill />,
+      },
+    ],
+  },
+]);
 
 export default function Router() {
   return <RouterProvider router={router} />;

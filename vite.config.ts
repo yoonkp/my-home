3716294554133ec70bfc5507367ce1024d004ad1
Yoonkyung Park <ygpark@nextree.io>
2025-02-11 22:30:00 +0900

    font fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,24 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-home/", // 리포지토리 이름을 포함한 경로 설정
+  base: "/my-home/", // GitHub Pages에 맞게 설정
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
   server: {
-    cors: true, // 개발 서버 CORS 설정
+    cors: true,
     host: "localhost",
     port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // 정적 파일이 저장될 폴더 지정
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
   },
 });
